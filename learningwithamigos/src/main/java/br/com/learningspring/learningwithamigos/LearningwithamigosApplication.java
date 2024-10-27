da70520e82b41a6e8b1e0c96f7bc5847ff76d937
Vinicius Sousa <viniciusps.1190@gmail.com>
2024-10-27 19:36:33 -0300

    Iniciando projeto

---
+++ learningwithamigos/src/main/java/br/com/learningspring/learningwithamigos/LearningwithamigosApplication.java
@@ -0,0 +1,13 @@
+package br.com.learningspring.learningwithamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningwithamigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningwithamigosApplication.class, args);
+	}
+
+}
