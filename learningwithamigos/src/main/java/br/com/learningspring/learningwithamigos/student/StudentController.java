003faec2a1cf488d8175238f10667d16d3e84fa3
Vinicius Sousa <viniciusps.1190@gmail.com>
2024-11-02 17:35:47 -0300

    Trying to understand a further more about beans and dependency injection

--- learningwithamigos/src/main/java/br/com/learningspring/learningwithamigos/student/StudentController.java
+++ learningwithamigos/src/main/java/br/com/learningspring/learningwithamigos/student/StudentController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
@@ -13,7 +11,7 @@ import java.util.List;
                                                 // This is a good practice to version the API.
 public class StudentController {
 
-    private final StudentService studentService; // This is a dependency injection, it is a good practice to use it. It makes the code more testable.
+    private final StudentService studentService; // This is a bean, it is managed by Spring. It is a dependency.
                                                     // It is a way to inject the dependency into the class.
 
     public StudentController(StudentService studentService) { // Constructor
@@ -25,5 +23,16 @@ public class StudentController {
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
+
+    // In those lines above, we are creating a controller that will return a list of students.
+
+    // In topics, we did:
+    // 1. Created a service that returns a list of students.
+    // 2. Created a controller that will return the list of students using the get Method of our service.
+    // 3. We used the @RestController annotation to indicate that this class is a controller.
+    // 4. We used the @RequestMapping annotation to indicate the path to access this controller.
+    // 5. We used the @GetMapping annotation to indicate that this method will be called when a GET request is made to the path.
+    // 6. We injected the service into the controller using the constructor.
+    // 7. We returned the list of students from the service.
 
 }
--- learningwithamigos/src/main/java/br/com/learningspring/learningwithamigos/student/StudentService.java
+++ learningwithamigos/src/main/java/br/com/learningspring/learningwithamigos/student/StudentService.java
@@ -1,12 +1,21 @@
 package br.com.learningspring.learningwithamigos.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-public class StudentService {
+// @Component // It declares that this class is a Spring bean.
+            // A bean is a complex concept, but in a simple way, it is a class that is managed by Spring.
+             // Being magaged by Spring means that Spring will create an instance of this class and will keep it in a container.
+              // Which means that we can inject this class into other classes. This is called dependency injection.
+                // Just like an object in a class, but it were created by Spring before.
+
+@Service // This annotation is used to indicate that this class is a service, which means that it will have business logic.
+            // @Service is a specialization of @Component, it will be served to other classes, like the Controller class.
+
+public class StudentService { // Now, with the notion of a service, we've declared this class as a service.
 
     public List<Student> getStudents() { // Bringing a list of Students defined in other package/class.
         return List.of(
